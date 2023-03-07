c52393f492d592414b6afcf512c9b8dc39c87b04
Tumelo Mosomane <84590318+Tumelo4@users.noreply.github.com>
2023-03-07 05:16:37 +0200

    fix private_key from .env issue

--- pages/api/webhook/webhook.ts
+++ pages/api/webhook/webhook.ts
@@ -19,13 +19,12 @@ const cors = Cors({
   allowMethods: ['POST', 'HEAD'],
 })
 
-const encode_private_key = process.env.private_key || ''
 
 const serviceAccountKey = {
   type: process.env.type || '',
   project_id:  process.env.project_id || '',
   private_key_id:  process.env.private_key_id || '',
-  private_key:  process.env.private_key ? Buffer.from(encode_private_key, 'base64').toString('ascii') : '',
+  private_key:  process.env.private_key || '',
   client_email:  process.env.client_email || '',
   client_id:  process.env.client_id || '',
   auth_uri:  process.env.auth_uri || '',
